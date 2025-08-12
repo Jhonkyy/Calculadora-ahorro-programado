a6b8fd45493344a1f3551c99202cfbc9216ac107
Cristian <cristian.andres.copete@hotmail.com>
2025-08-12 10:25:21 -0500

    se agregan los casos extraordinarios

--- test_ahorro.py
+++ test_ahorro.py
@@ -5,7 +5,7 @@ import unittest
 
 class testAhorro(unittest.TestCase):
     #Cada prueba es un metodo y DEBE iniciar coon la palabra "test"
-    
+    """CASOS NORMALES"""
     def test_normal1(self):
         #entradas
         meta = 1100000
@@ -41,6 +41,46 @@ class testAhorro(unittest.TestCase):
         #salidas
         cuota_esperada = 3333333.33
         self.assertAlmostEqual(cuota_calculado,cuota_esperada,2)
+        
+        """CASO EXTRAORDINARIOS"""
+        
+    def test_Extra1(self):
+        #entradas
+        meta = 0
+        plazo = 1
+        extra = 0
+        #proceso
+        
+        cuota_calculado = calcular_ahorro(meta,plazo,extra)
+        #salidas
+        cuota_esperada = 0
+        self.assertAlmostEqual(cuota_calculado,cuota_esperada,2)
+        
+    def test_Extra2(self):
+        #entradas
+        meta = 90000
+        plazo = 1
+        extra = 0
+        #proceso
+        
+        cuota_calculado = calcular_ahorro(meta,plazo,extra)
+        #salidas
+        cuota_esperada = 90000
+        self.assertAlmostEqual(cuota_calculado,cuota_esperada,2)
+
+        
+    def test_Extra3(self):
+        #entradas
+        meta = 400000
+        plazo = 2
+        extra = 400000
+        #proceso
+        
+        cuota_calculado = calcular_ahorro(meta,plazo,extra)
+        #salidas
+        cuota_esperada = 0
+        self.assertAlmostEqual(cuota_calculado,cuota_esperada,2)
+
 
 
 
