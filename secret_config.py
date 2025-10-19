16cb6664df025a9fb4cbe9373fe06c45455fc6db
cristian <rockstar22136262@gmail.com>
2025-10-19 18:23:38 -0500

    Guardando mis cambios antes del pull

---
+++ secret_config.py
@@ -0,0 +1,6 @@
+PGDATABASE = "ahorro_programado"
+PGUSER = "profe"
+PGPASSWORD = "BGlX54hhRw2BpwWd19I1FvLCL1Vbz5sj"
+PGHOST = "dpg-d3dvol3uibrs73a2o1h0-a.virginia-postgres.render.com"
+PGPORT = "5432"
+
